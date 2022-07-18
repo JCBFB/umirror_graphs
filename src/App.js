d2db396cf31a707d8ce939de6f4ac8f45382ffb3
JCBFB <juliocbfb123@gmail.com>
2022-07-18 11:13:08 -0300

    graphs pro site

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import BarChart from './Componentes/graf_barra';
+
+function App() {
+  return (
+    <div>
+      <BarChart/>
+    </div>
+  );
+}
+
+export default App;
